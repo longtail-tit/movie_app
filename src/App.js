8a58fac74f0ebc60e07b9f6deb3349407a130939
betti <jsy950601@gmail.com>
2019-12-12 20:33:33 +0900

    how to use "map ,array"

--- src/App.js
+++ src/App.js
@@ -3,32 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitle = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
+// 만약 대량의 영화 정보를 가져오고 싶을땐 비효율적인 방법임. 
+// => 정렬된 항목으로 (array)
+const movies = [
+  // api에서 찾은 정보를 배열할 수 있는 array
+  // 영화 - 제목, 포스터 
+  {
+    title: "Matrix",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0aMKrj8CQmPgSYhWfnV1tkaEIDuHu-mLNypNjEs8dgxNjst3h&s"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "http://mblogthumb1.phinf.naver.net/MjAxOTAxMDNfMSAg/MDAxNTQ2NTIyNzIxMDQy.Mg-Unq0TcP20nuJt4jwQIII0qaJoxf9blEmWZryiL_Ig.Iya4LSd-MU81saiCOXhBAAaF87TMoWCTfO-pz_WjyI4g.JPEG.winpil99/full.jpg?type=w800"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPIxcRSI3uJNGwJIhZW-gyeAbQemnlnL3cdBl7M9F2zTmkpcrG&s"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://pbs.twimg.com/media/ED5kOUeWsAEGBDL.jpg"
+  }
 ]
-//영화들의 제목을 자식 컴포넌트인 movie컴포넌트에 보내고 싶다면 ?
-
-const movieImages=[
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0aMKrj8CQmPgSYhWfnV1tkaEIDuHu-mLNypNjEs8dgxNjst3h&s",
-  "http://mblogthumb1.phinf.naver.net/MjAxOTAxMDNfMSAg/MDAxNTQ2NTIyNzIxMDQy.Mg-Unq0TcP20nuJt4jwQIII0qaJoxf9blEmWZryiL_Ig.Iya4LSd-MU81saiCOXhBAAaF87TMoWCTfO-pz_WjyI4g.JPEG.winpil99/full.jpg?type=w800",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPIxcRSI3uJNGwJIhZW-gyeAbQemnlnL3cdBl7M9F2zTmkpcrG&s",
-  "https://pbs.twimg.com/media/ED5kOUeWsAEGBDL.jpg"
-
-]
-// 메인컴포넌트가 주소를 다 가지고 있고 차일드 컴포넌트에서는 this.props.poster로만 사용해주면 된다. 
-//즉 데이터 플로우 -> 큰 컴포넌트가 전체 정보를 다 가지고있고 각기 칠드런에게 정보를 전달한다. 이를 이용해서 강력한 UI를 구축할 수 있다. 
 class App extends  Component {
   render() {
     return (
       <div className="App">
-    {/* 모든 영화 내용, 정보가 들어갈 곳 */}
-      <Movie  title={movieTitle[0]} poster={movieImages[0]} />
-      <Movie  title={movieTitle[1]} poster={movieImages[1]}/>
-      <Movie  title={movieTitle[2]} poster={movieImages[2]}/>
-      <Movie  title={movieTitle[3]} poster={movieImages[3]}/>
+{/* array의 map 기능
+movies는 array , 그 다음 map 기능을 통해서 새로운 array를 만듬 
+=> movies array를 가져다가 매핑을 해서 새로운 array를 만드는 것. 해당 movies array 안의 엘러먼트를 활용해서.
+현재 작업하고있는 movies array   를 활용한다는 것이 포인트이다. 
+*/}
+        {movies.map(movie => {
+          // 위의 movies array의 엘리먼트를 토대로한 컴포넌트
+          return <Movie title={movie.title} poster={movie.poster}/>
+        })}
+        {/* {[
+          <Movie title={movies[0].title} poster={movies[0].poster} />
+          <Movie title={movies[1].title} poster={movies[1].poster} />
+          <Movie title={movies[2].title} poster={movies[2].poster} />
+          <Movie title={movies[3].title} poster={movies[3].poster} />
+        ]} */}
       </div>
     )
 
