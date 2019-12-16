2e0a3b71fd4ed6a571013b12bb3d32e0b5a85daf
betti <jsy950601@gmail.com>
2019-12-16 20:32:42 +0900

    CSS for Movie

--- src/App.js
+++ src/App.js
@@ -1,70 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Movie from './Movie';
-
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state={}
+  state = {};
 
-  componentDidMount(){  // <= 사이즈가 커지면 좋은 코딩이 아님. 왜냐면 많은 펑션을 불러올거라서 펑션들이 한 군데에 있는것보다 다른 장소에 잇는게 좋음 
+  componentDidMount() {
     this._getMovies();
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.large_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
 
-
- _renderMoview = () => { 
-  const movies = this.state.movies.map( movie  => {  // index 대신 movie.id 사용 => component에서 index 사용하면 느림 
-    console.log(movie)
-    return <Movie 
-        title={movie.title_english} 
-        poster={movie.medium_cover_image} 
-        key={movie.id} 
-        genres={movie.genres}
-        synopsis={movie.synopsis}
-      />
-   }) 
-   return movies
-  }
- 
-  //async function? 이전 라인의 작업이 끝날때까지 기다리는 것이 아닐 때 , 순서 노상관 
-  //await 한다는것?  callApi기능이 끝나는 것을 기다리고, 그것의 return value(그것이 무엇이던)를 movies변수에 넣는것이다. 
-   _getMovies = async () => {
-    const movies = await this._callApi() // await모드에서 callApi 펑션 밸류를 갖는 moves 변수 선언 
-    this.setState({ //<= callApi 작업이 완료되기 전 까지는 실행되지 않는다.
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
       movies
-    })
-  }
+    });
+  };
 
   _callApi = () => {
-    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=like_count')
-    .then(potato => potato.json())          
-    .then(json =>  json.data.movies)   // '=>' 모던 자바스크립트라서 화살표 자체에 return 이라는 뜻이 내재되어 있다. 
-    .catch(err => console.log('err')) 
-  }
+    return fetch(
+      "https://yts.lt/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {/* Loading State */}
-        {this.state.movies ? this._renderMoview() : 'Loading'} 
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
-    )
-
+    );
   }
 }
 
-//*정리*
-// 1. fetch 를 call api 로 변경 
-// 2. get movies라는 function 사용 (asynchronous function )
-// 3. ㄴ 안에 movies라는 이름의 const variable을 생성 
-// 4. call api 작업이 완료되고 return 하기를 await
-// 5. call api 는 fetch promise를 return할 것인데, 이는 모든 데이터의 제이슨이다.
-// 6. json.data.movies 라는 value는 const movies의 결과값이 된다. 
-// 7. get movies 함수 안에서 this.setState를 movies로 설정 
+export default App;
 
-// async 안쓰면 await 작동 안한다 
-
-export default App
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,67 +1,59 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
-function Movie({title, poster, genres, synopsis}){ //<== 사용할 prop 정의
-    return(  // JSK에서는 일반적인 class 를 className이라고 쓴다. 
-        <div className="Movie">  
-            <div className="Movie__Columns" >
-               <MoviePoster poster={poster} alt={title}/>
-            </div>
-            <div className="Movie__Columns" >
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title} />
+            </div>   
+            <div className="Movie__Column">
                 <h1>{title}</h1>
-                    <div className="Movie__Genres">
-                       {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
-                       {/* 장르 array 매핑 */}
-                    </div>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <div className="Movie__Synopsis">
+                <LinesEllipsis
+                    text={synopsis}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                    />   
+                </div>
             </div>
-            <p className="Movie__Synosis" >
-                {synopsis}
-            </p>
         </div>
-       
     )
 }
 
-
-function MoviePoster({poster,alt}){
-    return( // alt prop을 추가했더니...이미지에 커서 올리면 title 띄워짐 
-        <img  src={poster} alt={alt} title={alt} className="Movie__Poster"/>
-        )
-    }
+function MoviePoster({poster, alt}){
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    )
+}
 
 function MovieGenre({genre}){
-        return(
-        <span className="Movie__Genres">{genre} </span>
-        )
-    }
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
 
-
-
-//prop types check
 Movie.propTypes = {
-    title: propTypes.string.isRequired,
-    poster : propTypes.string.isRequired,
-    genres : propTypes.array.isRequired,
-    synopsis :propTypes.string.isRequired
-}     
-    
-//How can check prop types?
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
 MoviePoster.propTypes = {
-    poster : propTypes.string.isRequired,
-    alt : propTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
 }
 
 MovieGenre.propTypes ={
-    genre : propTypes.string.isRequired
+    genre: PropTypes.string.isRequired
 }
-
-//*정리* 
-// props 추가 (genre, synosis, alt)
-// propTypes에 regist -> 체크할 수 있도록 
-// html 클래스명 추가 
-// 컬럼 - 무비포스터, 제목, 장르 array 매핑, 무비시놉시스 클래스 
-// 이미지의 제목이 뜨도록 alt 이미지 만듦
-// 장르를 맵핑할 때 movie renre라는 새로운 컴포넌트를 만들었다.<= functional 컴포넌트로 간단한 span 을 return 하게끔 
 
 export default Movie
